9246f135fd2f28e0e6f833616aa7370e9158af18
Mario Markovic <velebit.business@gmail.com>
2020-03-16 13:28:52 +0100

    Finished step 3.

--- src/ValidationComponent/ValidationComponent.js
+++ src/ValidationComponent/ValidationComponent.js
@@ -2,9 +2,34 @@ import React from 'react';
 import './ValidationComponent.css';
 
 const ValidationComponent = (prop) => {
+
+  let len = prop.txtLength;
+  let txt = null;
+  let componentStyle = null;
+
+  if (len < 5) {
+    txt = 'Text too small.';
+    componentStyle = {
+      display: 'inline-block',
+      margin: '3px',
+      border: '3px solid red',
+      padding: '3px',
+      backgroundColor: 'rgb(255, 104, 104)'
+    }
+  } else {
+    txt = 'Text long enough.';
+    componentStyle = {
+      display: 'inline-block',
+      margin: '3px',
+      border: '3px solid green',
+      padding: '3px',
+      backgroundColor: 'lightgreen'
+    }
+  }
+
   return (
-    <div className="ValidationComponent">
-      {prop.txtLength}
+    <div className="ValidationComponent" style={componentStyle}>
+      {txt}
     </div>
   );
 }
